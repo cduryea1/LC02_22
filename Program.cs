63e57f40069fedae664a41fefb50517acf76aabc
cduryea1 <157415354+cduryea1@users.noreply.github.com>
2024-02-21 13:05:44 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿//mis 3033
+//lc 2-19
+//113529005
+//camille duryea
+
+using LC0219.Data;
+using LC0219.Models;
+using System.Text.Json;
+
+Console.WriteLine("Existing Database");
+
+StudentDB db; //StudentDB, complex, expensive
+db = new StudentDB();//load the Db into file
+//table:collection(array, list, iEnumerable)
+//row:
+
+//var r = db.Students.ToList();//
+//where (): work on table, result
+
+//var r = db.Students.Where(x=>x.LetterGrade=="A").ToList();//List<Student>
+var r = db.Students.Where(x => x.LetterGrade == "A" && x.Grade>95).ToList();//List<Student>
+
+var r2 = db.Students.Where(a => a.LetterGrade == "B").FirstOrDefault();
+if (r2 != null)
+{
+    Console.WriteLine(r2);
+}
+else
+{
+    Console.WriteLine("No records!");
+}
+//select() work on table, result in table
+
+
+
+for (int i = 0; i < r.Count; i++)
+{
+    r[i].GetLG();
+    Console.WriteLine(r[i]);//
+    //Console.WriteLine(r[i].ToString());
+}
+db.SaveChanges(); //persist, save data from ram to harddrive file
+//[]:table
+//{}: row
+var r3 = db.Students.Select(x => new {Grade=x.Grade, LG = x.LetterGrade });
+//OrderBy() orderbyDesceding
+//OrderBy() work on table, result in table
+//ascending
+var r4 = db.Students.OrderBy(b => b.Grade);
+
+//MaxBy(), MinBy() work on table
+//will only work on list
+var r5 = db.Students.ToList().MaxBy(a => a.Grade);
+
+
+
+
+JsonSerializerOptions opts = new JsonSerializerOptions();
+opts.WriteIndented = true;
+
+string dataStr = JsonSerializer.Serialize(r5, opts);
+Console.WriteLine(dataStr);
+File.WriteAllText("data.json", dataStr);
+
+//average():double Sum(): Count()
+double aveGrade = db.Students.Average(x => x.Grade);
+Console.WriteLine(aveGrade);
+
+double sumGrade = db.Students.Sum(x => x.Grade);
+Console.WriteLine(sumGrade);
+
+int c = db.Students.Where(x => x.LetterGrade == "C").Count();
+Console.WriteLine(c);
---
+++ Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace LC0219.Models;
+
+[Table("students")]
+public partial class Student:Object// inherit
+{
+    [Key]
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public double Grade { get; set; }
+
+    public string LetterGrade { get; set; } = null!;
+
+    public override string ToString()
+    {
+        return $"Name:{this.Name},Grade:{this.Grade:F2},LG:{this.LetterGrade}";//string.Format($"")
+
+    }
+    public string GetLG()
+    {
+        if (this.Grade >= 90)
+        {
+            this.LetterGrade = "A";
+        }
+        else if(this.Grade >= 80)
+        {
+            this.LetterGrade = "B";
+        }
+        else if(this.Grade >= 70)
+        {
+            this.LetterGrade = "C";
+        }
+        else if(this.Grade >=60)
+        {
+            this.LetterGrade = "D";
+        }
+        else
+        {
+            this.LetterGrade = "F";
+        }
+        return this.LetterGrade;
+    }
+}
